d741148a44d4a1ef61a5a7567ad505c15342ffc3
Vaibhav Sanjay Bichave <79131292+VaibhavBichave@users.noreply.github.com>
2023-09-03 00:57:38 +0530

    Added solution - LeetHub

---
+++ Pair Sum Existence - GFG/pair-sum-existence.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int sumExists(int arr[], int n, int sum);
+    
+int main() {
+	
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    
+	    int arr[n];  //array of size n
+            
+        for(int i = 0; i < n; i++)
+            cin>>arr[i]; //Input the array
+             
+        int sum; 
+        cin>>sum;
+          
+        cout<< sumExists(arr, n, sum) <<endl;    
+            
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for C++
+
+int sumExists(int arr[], int n, int sum){
+    unordered_map<int, int> seen;
+    for (int i = 0; i < n; ++i) {
+      if (seen.find(arr[i]) != seen.end()) return 1;
+      seen[sum - arr[i]] = i;
+    }
+    return 0; 
+    
+}
