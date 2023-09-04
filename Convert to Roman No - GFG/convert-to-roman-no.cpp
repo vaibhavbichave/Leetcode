0772e66d70e24b2cd70b4559b4e662c0a411334c
Vaibhav Sanjay Bichave <79131292+VaibhavBichave@users.noreply.github.com>
2023-09-05 00:35:49 +0530

    Added solution - LeetHub

---
+++ Convert to Roman No - GFG/convert-to-roman-no.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+  public:
+    string convertToRoman(int n) {
+        string answer;
+        int num[] = {1,4,5,9,10,40,50,90,100,400,500,900,1000};
+        string sym[] = {"I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"};
+        for(int i=12;i>=0;i--){
+            int div = n / num[i];
+            n = n % num[i];
+            while(div--) answer += sym[i];
+        }
+        return answer;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int N;
+		cin>>N;
+		Solution ob;
+    	cout << ob.convertToRoman(N) << endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
