f00a8bfa9f5435996f670be442ab2bb098906fd2
Vaibhav Bichave <79131292+VaibhavBichave@users.noreply.github.com>
2023-10-04 00:05:51 +0530

    Added solution - LeetHub

--- Column name from a given column number - GFG/column-name-from-a-given-column-number.cpp
+++ Column name from a given column number - GFG/column-name-from-a-given-column-number.cpp
@@ -7,30 +7,23 @@ using namespace std;
 
 class Solution{
     public:
-    string alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     string colName (long long int n)
     {
-       char str[100000]; // To store result (Excel column name)
-        int i = 0; // To store current index in str which is result
+        char str[100];
+        int i = 0;
      
         while (n > 0) {
-            // Find remainder
             int rem = n % 26;
-     
-            // If remainder is 0, then a 'Z' must be there in output
             if (rem == 0) {
                 str[i++] = 'Z';
                 n = (n / 26) - 1;
             }
-            else // If remainder is non-zero
-            {
+            else{
                 str[i++] = (rem - 1) + 'A';
                 n = n / 26;
             }
         }
         str[i] = '\0';
-     
-        // Reverse the string and print result
         reverse(str, str + strlen(str));
         return str;
     
