16599715890705564e8a4fb559c8341e0f3748a2
Vaibhav Bichave <79131292+VaibhavBichave@users.noreply.github.com>
2023-10-15 11:22:02 +0530

    Added solution - LeetHub

--- Cyclically rotate an array by one - GFG/cyclically-rotate-an-array-by-one.cpp
+++ Cyclically rotate an array by one - GFG/cyclically-rotate-an-array-by-one.cpp
@@ -29,11 +29,9 @@ int main()
 
 //User function Template for C++
 
-void reverse(int arr[], int i, int j){
-    while(i<j) swap(arr[i++],arr[j--]);
-}
 void rotate(int arr[], int n)
 {
-    reverse(arr, 0, n-1);
-    reverse(arr, 1, n-1);
+    int x = arr[n-1];
+    for(int i=n-1;i>0;i--) arr[i] = arr[i-1];
+    arr[0] = x;
 }
