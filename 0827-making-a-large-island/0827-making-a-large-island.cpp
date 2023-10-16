c79980e22a894c48e02bbd19b127e63da6ed70c0
Vaibhav Bichave <79131292+VaibhavBichave@users.noreply.github.com>
2023-10-16 22:57:52 +0530

    Time: 766 ms (12.91%), Space: 312.8 MB (6.86%) - LeetHub

---
+++ 0827-making-a-large-island/0827-making-a-large-island.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int dfs(vector<vector<int>>& grid, int i, int j, int index) {
+        int n=grid.size();
+        if(i<0 || i>=n || j<0 || j>=n || grid[i][j]!=1) return 0;
+        grid[i][j]=index;
+        int count = dfs(grid, i+1, j, index) + 
+                    dfs(grid, i-1, j, index) + 
+                    dfs(grid, i, j+1, index) + 
+                    dfs(grid, i, j-1, index);
+        return count+1;
+    }
+    
+    void findNeighbours(vector<vector<int>>& grid, int i, int j, unordered_set<int>& s) {
+        int n=grid.size();
+        if(i>0) s.insert(grid[i-1][j]);
+        if(j>0) s.insert(grid[i][j-1]);
+        if(i<n-1) s.insert(grid[i+1][j]);
+        if(j<n-1) s.insert(grid[i][j+1]);
+    }
+    
+    int largestIsland(vector<vector<int>>& grid) {
+        int n=grid.size(), res=0, index=2, count;
+        unordered_map<int, int> area;
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<n;j++) {
+                if(grid[i][j]==1) {
+                    area[index]=dfs(grid, i, j, index);
+                    res=max(res, area[index++]);
+                }
+            }
+        }
+
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<n;j++) {
+                if(grid[i][j]==0) {
+                    count = 1; 
+                    unordered_set<int> s;
+                    findNeighbours(grid, i, j, s);
+                    for(int i:s) count+=area[i]; 
+                    res=max(res, count);
+                }
+            }
+        }
+        
+        return res;
+    }
+};
