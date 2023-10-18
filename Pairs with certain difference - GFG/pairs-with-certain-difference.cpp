9b660bb46298b9b80b73eded3130997b4e55b332
Vaibhav Bichave <79131292+VaibhavBichave@users.noreply.github.com>
2023-10-18 23:21:14 +0530

    Added solution - LeetHub

---
+++ Pairs with certain difference - GFG/pairs-with-certain-difference.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    int maxSumPairWithDifferenceLessThanK(int arr[], int N, int K)
+    {
+        int result = 0;
+        sort(arr, arr+N);
+        for(int i=N-1;i>=1;i-=2){
+           if((arr[i]-arr[i-1])<K) result = result+arr[i]+arr[i-1];
+           else i++;
+        }
+        return result;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int N;
+		cin>>N;
+		int arr[N];
+		for(int i=0;i<N;i++)
+		cin>>arr[i];
+
+		int K;
+		cin>>K;
+        Solution ob;
+		cout<<ob.maxSumPairWithDifferenceLessThanK(arr,N,K)<<endl;
+	}
+	return 0;
+}
+
+// } Driver Code Ends
