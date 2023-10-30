7a13ed38148d808109f9f21db3d689fb6e6ea871
Vaibhav Bichave <79131292+vaibhavbichave@users.noreply.github.com>
2023-10-30 23:59:33 +0530

    Added solution - LeetHub

--- Sum of XOR of all pairs - GFG/sum-of-xor-of-all-pairs.cpp
+++ Sum of XOR of all pairs - GFG/sum-of-xor-of-all-pairs.cpp
@@ -15,26 +15,15 @@ class Solution{
     long long int sumXOR(int arr[], int n)
     {
     	 long long int sum = 0;
-        for (int i = 0; i < 32; i++) 
-        {
-            //  Count of zeros and ones
-            long long  zc = 0, oc = 0; 
-             
-            // Individual sum at each bit position
+        for (int i = 0; i < 32; i++) {
+            long long  zc = 0, oc = 0;
             long long int idsum = 0; 
-            for (int j = 0; j < n; j++)
-            {
-                if (arr[j] % 2 == 0)
-                    zc++;
-                else
-                    oc++;
-                arr[j] /= 2;
+            for (int j = 0; j < n; j++){
+                if (arr[j] & 1) oc++;
+                else zc++;
+                arr[j] >>= 1;
             }
-             
-            // calculating individual bit sum 
             idsum = oc * zc * (1 << i); 
-     
-            // final sum    
             sum += idsum; 
         }
         return sum;
