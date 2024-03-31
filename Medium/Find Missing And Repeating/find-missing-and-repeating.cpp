2b872bcf5ebf66d23eef9dce9a563478bcebf7e8
Vaibhav Bichave <79131292+vaibhavbichave@users.noreply.github.com>
2024-03-31 22:33:40 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Find Missing And Repeating/find-missing-and-repeating.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+    vector<int> findTwoElement(vector<int> arr, int n) {
+        int duplicate = -1, missing = -1;
+        for (int i = 0; i < n; i++) {
+            if (arr[abs(arr[i]) - 1] > 0) arr[abs(arr[i]) - 1] = -arr[abs(arr[i]) - 1];
+            else duplicate = abs(arr[i]);
+        }
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > 0) missing = i + 1;
+        }
+        return {duplicate, missing};
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        Solution ob;
+        auto ans = ob.findTwoElement(a, n);
+        cout << ans[0] << " " << ans[1] << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
