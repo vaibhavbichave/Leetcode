505defb57668fe8b34a4812d4f7119b538f51df3
Vaibhav Bichave <79131292+vaibhavbichave@users.noreply.github.com>
2024-06-15 17:38:59 +0530

    Time: 118 ms (96.03%), Space: 85.6 MB (42.83%) - LeetHub

--- 0502-ipo/0502-ipo.cpp
+++ 0502-ipo/0502-ipo.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
-    static bool cmp(pair<int, int>& a, pair<int, int>& b) {
-        if (a.second == b.second) return a.first < b.first;
-        return a.second < b.second;
-    }
     int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
         int n = profits.size();
-        vector<pair<int, int>> v;
-        for (int i = 0; i < n; i++) v.push_back({profits[i], capital[i]});
-        sort(v.begin(), v.end(), cmp);
-        priority_queue<pair<int, int>> pq;
-        for (int i = 0, j = 0; i < k; i++) {
-            while (j < n && v[j].second <= w) pq.push(v[j++]);
-            if (pq.empty()) break;
-            w += pq.top().first;
-            pq.pop();
+        vector<pair<int, int>> projects;
+        for (int i = 0; i < n; ++i) {
+            projects.emplace_back(capital[i], profits[i]);
+        }
+        sort(projects.begin(), projects.end());
+        priority_queue<int> maxHeap;
+        for (int i = 0, j = 0; j < k; ++j) {
+            while (i < n && projects[i].first <= w) {
+                maxHeap.push(projects[i++].second);
+            }
+            if (maxHeap.empty()) {
+                break;
+            }
+            w += maxHeap.top();
+            maxHeap.pop();
         }
         return w;
     }
