187762812b3e8af0f864f017f3e132dd25ecf89b
Vaibhav Bichave <79131292+vaibhavbichave@users.noreply.github.com>
2024-07-20 15:35:16 +0530

    Time: 31 ms (88.26%), Space: 36.2 MB (11.51%) - LeetHub

--- 1711-find-valid-matrix-given-row-and-column-sums/1711-find-valid-matrix-given-row-and-column-sums.cpp
+++ 1711-find-valid-matrix-given-row-and-column-sums/1711-find-valid-matrix-given-row-and-column-sums.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
-    vector<vector<int>> restoreMatrix(vector<int>& rowSum, vector<int>& colSum) {
-        int r = rowSum.size(), c = colSum.size();
-        vector<vector<int>> arr(r, vector<int>(c, 0));
-        for (int i = 0, j = 0; i < r && j < c; i += (rowSum[i] == 0), j += (colSum[j] == 0)) {
-            int x = min(rowSum[i], colSum[j]);
-            arr[i][j] = x;
-            rowSum[i] -= x;
-            colSum[j] -= x;
+    vector<vector<int>> restoreMatrix(vector<int>& rowSum,
+                                      vector<int>& colSum) {
+        int n = rowSum.size(), m = colSum.size();
+        vector<vector<int>> result(n, vector<int>(m, 0));
+        for (int i = 0, j = 0; i < n && j < m;
+             i += (rowSum[i] == 0), j += (colSum[j] == 0)) {
+            int mini = min(rowSum[i], colSum[j]);
+            result[i][j] = mini;
+            rowSum[i] -= mini;
+            colSum[j] -= mini;
         }
-        return arr;
+        return result;
     }
 };
