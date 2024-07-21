f6518f01df03bb32b12d8a8cb46b5d01cc67cc83
Vaibhav Bichave <79131292+vaibhavbichave@users.noreply.github.com>
2024-07-21 11:39:10 +0530

    Time: 9 ms (21.49%), Space: 14.3 MB (17.81%) - LeetHub

--- 1720-crawler-log-folder/1720-crawler-log-folder.cpp
+++ 1720-crawler-log-folder/1720-crawler-log-folder.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     int minOperations(vector<string>& logs) {
-        int count = 0;
-        for (string x : logs) {
-            if (x[0] == '.' && x[1] == '.') {
-                if (count > 0) {
-                    count--;
-                }
-            } else if (x[0] != '.') {
-                count++;
+        int level = 0;
+        for (auto& dir : logs) {
+            if (dir == "../") {
+                level -= (level > 0);
+            } else if (dir != "./") {
+                level++;
             }
         }
-        return count;
+        return level;
     }
 };
