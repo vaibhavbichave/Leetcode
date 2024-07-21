a9e0080d3b559fea30525f59200be6eb13b4f02f
Vaibhav Bichave <79131292+vaibhavbichave@users.noreply.github.com>
2024-07-21 17:52:22 +0530

    Time: 52 ms (56.18%), Space: 43.2 MB (24.32%) - LeetHub

---
+++ 3335-minimum-operations-to-write-the-letter-y-on-a-grid/3335-minimum-operations-to-write-the-letter-y-on-a-grid.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int minimumOperationsToWriteY(vector<vector<int>>& grid) {
+        int n = grid.size();
+        unordered_map<int, int> freqOnY;
+        unordered_map<int, int> freqNotOnY;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i < n / 2) {
+                    if (i == j || i == (n - j - 1)) {
+                        freqOnY[grid[i][j]]++;
+                    } else {
+                        freqNotOnY[grid[i][j]]++;
+                    }
+                } else {
+                    if (j == n / 2)
+                        freqOnY[grid[i][j]]++;
+                    else {
+                        freqNotOnY[grid[i][j]]++;
+                    }
+                }
+            }
+        }
+
+        int ans = INT_MAX;
+        int ycount = 3 * (n / 2) + 1;
+        int nonYcount = n * n - ycount;
+        for (int i = 0; i < 3; i++) {
+            int toYmake = ycount - freqOnY[i];
+            int toNonY = INT_MAX;
+            for (int j = 0; j < 3; j++) {
+                if (i != j) {
+                    toNonY = min(toNonY, nonYcount - freqNotOnY[j]);
+                }
+            }
+            ans = min(ans, toYmake + toNonY);
+        }
+        return ans;
+    }
+};
