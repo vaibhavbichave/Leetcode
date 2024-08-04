7230ec1b8dd9eb65b9fddb7e3e15c0ebbdfbc717
Vaibhav Bichave <79131292+vaibhavbichave@users.noreply.github.com>
2024-08-04 22:13:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Elements in the Range/elements-in-the-range.cpp
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+	
+	
+	public:
+	bool check_elements(int arr[], int n, int A, int B)
+	{
+		unordered_set<int>s;
+		for(int i=0;i<n;i++) s.insert(arr[i]);
+		for(int i=A;i<=B;i++){
+		    if(s.find(i)==s.end()){
+		        return false;
+		    }
+		}
+		return true;
+	}
+};
+	
+
+//{ Driver Code Starts.
+
+
+int main() 
+{
+   	
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+    	int n,A,B;
+		cin>>n>>A>>B;
+		int a[n];
+		for(int i=0;i<n;++i)
+			cin>>a[i];
+
+        
+
+        Solution ob;
+        if (ob.check_elements(a, n, A, B))
+			cout << "Yes";
+		else
+			cout << "No";
+	    
+        
+	    cout << "\n";
+	     
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+// } Driver Code Ends
