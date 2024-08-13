e81181c226669cd20233bed5c6f37ddaad2cbe0f
Vaibhav Bichave <79131292+vaibhavbichave@users.noreply.github.com>
2024-08-13 20:49:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Maximum money/maximum-money.cpp
@@ -0,0 +1,28 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int maximizeMoney(int N , int K) {
+        int x = N&1?N/2+1:N/2;
+        return x*K;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N,K;
+
+        cin>>N>>K;
+
+        Solution ob;
+        cout << ob.maximizeMoney(N,K) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
