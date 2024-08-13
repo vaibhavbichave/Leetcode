6493a7b46c51289bc31a524c9c1c46259e714e63
Vaibhav Bichave <79131292+vaibhavbichave@users.noreply.github.com>
2024-08-13 09:34:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Angle between hour and minute hand/angle-between-hour-and-minute-hand.cpp
@@ -0,0 +1,30 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int getAngle(int H , int M) {
+        H = ((H - 12) + 12)% 12;
+        float angle = abs(0.5 * (H * 60 - 11 * M)); 
+        angle = min(360 - angle, angle); 
+        return angle; 
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int H,M;
+        
+        cin>>H>>M;
+
+        Solution ob;
+        cout << ob.getAngle(H,M) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
