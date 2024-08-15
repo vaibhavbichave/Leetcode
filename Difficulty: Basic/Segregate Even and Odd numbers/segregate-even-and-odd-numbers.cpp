f6d64af8da15dba2e8b4094782d80b3f17df8572
Vaibhav Bichave <79131292+vaibhavbichave@users.noreply.github.com>
2024-08-15 22:31:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Segregate Even and Odd numbers/segregate-even-and-odd-numbers.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    void segregateEvenOdd(vector<int>& arr) {
+        for(int &x: arr) if(x&1) x+=1e7;
+        sort(arr.begin(), arr.end());
+        for(int &x: arr) if(x&1) x-=1e7;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        obj.segregateEvenOdd(arr);
+        for (int i = 0; i < arr.size(); i++) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
