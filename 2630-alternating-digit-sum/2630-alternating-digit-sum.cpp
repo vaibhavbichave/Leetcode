f56a97269d5b23bb08c27be3baa31eb58f9b8e9c
Vaibhav Bichave <79131292+vaibhavbichave@users.noreply.github.com>
2024-12-22 00:46:55 +0530

    Time: 0 ms (100%), Space: 7.9 MB (31.35%) - LeetHub

--- 2630-alternating-digit-sum/2630-alternating-digit-sum.cpp
+++ 2630-alternating-digit-sum/2630-alternating-digit-sum.cpp
@@ -1,20 +1,12 @@
 class Solution {
 public:
     int alternateDigitSum(int n) {
-        int res1 = 0, res2 = 0;
-        int count = 0, odd = 0, even = 0;
+        int res = 0, sign = 1;
         while (n) {
-            int digit = n % 10;
-            if (count & 1) {
-                res1 += digit;
-                res2 -= digit;
-            } else {
-                res1 -= digit;
-                res2 += digit;
-            }
+            sign = -sign;
+            res += sign * n % 10;
             n /= 10;
-            count++;
         }
-        return count & 1 ? res2 : res1;
+        return sign * res;
     }
 };
