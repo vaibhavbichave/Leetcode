ef4d668a5e32f97cd02fa49567d4cefb2a329ede
Vaibhav Bichave <79131292+vaibhavbichave@users.noreply.github.com>
2024-12-26 23:56:52 +0530

    Time: 16 ms (98.26%), Space: 88.4 MB (11.68%) - LeetHub

--- 2188-minimized-maximum-of-products-distributed-to-any-store/2188-minimized-maximum-of-products-distributed-to-any-store.cpp
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/2188-minimized-maximum-of-products-distributed-to-any-store.cpp
@@ -1,23 +1,15 @@
 class Solution {
 public:
-    bool canDistribute(int x, vector<int>& quantities, int n) {
-        int j = 0, m = quantities.size();
-        int remaining = quantities[j];
-        for (int i = 0; i < n; i++) {
-            if (remaining <= x && ++j) {
-                if (j == m) return true;
-                remaining = quantities[j];
-            } else {
-                remaining -= x;
-            }
-        }
-        return false;
+    bool canDistribute(vector<int>& quantities, int target, int n) {
+        long long stores = 0;
+        for (int quantity : quantities) stores += (quantity + target - 1) / target;
+        return stores <= n;
     }
     int minimizedMaximum(int n, vector<int>& quantities) {
-        int left = 0, right = INT_MAX;
+        int left = 1, right = *max_element(quantities.begin(), quantities.end());
         while (left < right) {
             int middle = left + (right - left) / 2;
-            if (canDistribute(middle, quantities, n)) right = middle;
+            if (canDistribute(quantities, middle, n)) right = middle;
             else left = middle + 1;
         }
         return right;
