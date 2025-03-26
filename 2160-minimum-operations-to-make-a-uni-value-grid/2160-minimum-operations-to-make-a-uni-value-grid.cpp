a789e34c3eafa6edd5af580f97180bf1412d7efc
Vaibhav Bichave <79131292+vaibhavbichave@users.noreply.github.com>
2025-03-26 08:30:20 +0530

    Time: 47 ms (42.86%), Space: 89.9 MB (62.01%) - LeetHub

--- 2160-minimum-operations-to-make-a-uni-value-grid/2160-minimum-operations-to-make-a-uni-value-grid.cpp
+++ 2160-minimum-operations-to-make-a-uni-value-grid/2160-minimum-operations-to-make-a-uni-value-grid.cpp
@@ -1,25 +1,18 @@
 class Solution {
 public:
     int minOperations(vector<vector<int>>& grid, int x) {
-        vector<int> v;
-        int n = grid.size(), m = grid[0].size();
-        int rem = grid[0][0] % x, total = 0, sum = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] % x != rem) {
-                    return -1;
-                }
-                total += grid[i][j];
-                v.push_back(grid[i][j]);
-            }
+        vector<int> arr;
+        for (auto& row : grid)
+            for (int num : row)
+                arr.push_back(num);
+        sort(arr.begin(), arr.end());
+        int operations = 0, median = arr[arr.size() / 2];
+        for (int num : arr) {
+            int diff = abs(num - median);
+            if (diff % x)
+                return -1;
+            operations += diff / x;
         }
-        int result = INT_MAX;
-        sort(v.begin(), v.end());
-        for (int i = 0; i < m * n; i++) {
-            result = min(result, (v[i] * i - sum) / x +
-                                     (total - sum - v[i] * (n * m - i)) / x);
-            sum += v[i];
-        }
-        return result;
+        return operations;
     }
 };
