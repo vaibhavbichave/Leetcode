562c3b43afea2a60ecdb969f18232d00ee8359b5
Vaibhav Bichave <79131292+vaibhavbichave@users.noreply.github.com>
2025-03-27 09:30:51 +0530

    Time: 4 ms (77.46%), Space: 89.9 MB (85.66%) - LeetHub

--- 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.cpp
+++ 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     int minimumIndex(vector<int>& nums) {
         int n = nums.size();
-        unordered_map<int, int> firstMap, secondMap;
-        for (auto& num : nums) 
-            secondMap[num]++;
-        for (int index = 0; index < n; index++) {
-            int num = nums[index];
-            secondMap[num]--;
-            firstMap[num]++;
-            if (firstMap[num] * 2 > index + 1 &&
-                secondMap[num] * 2 > n - index - 1) {
-                return index;
-            }
+        int cnt = 0, dominant = 0;
+        for (int num : nums) {
+            dominant = (cnt == 0) ? num : dominant;
+            cnt += (num == dominant) ? 1 : -1;
+        }
+        int total_dominant = count(nums.begin(), nums.end(), dominant);
+        for (int i = 0, local_dominant = 0; i < n; i++) {
+            local_dominant += (nums[i] == dominant);
+            if (local_dominant * 2 > (i + 1) &&
+                ((total_dominant - local_dominant) * 2 > (n - i - 1)))
+                return i;
         }
         return -1;
     }
