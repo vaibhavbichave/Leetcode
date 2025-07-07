507ed13ecef3fd67d8c6f875512c1568ebd4a37d
Vaibhav Bichave <79131292+vaibhavbichave@users.noreply.github.com>
2025-07-07 22:27:29 +0530

    Time: 12 ms (98.4%), Space: 50.4 MB (96.08%) - LeetHub

--- 1663-detect-cycles-in-2d-grid/1663-detect-cycles-in-2d-grid.cpp
+++ 1663-detect-cycles-in-2d-grid/1663-detect-cycles-in-2d-grid.cpp
@@ -1,46 +1,29 @@
 class Solution {
 public:
-    bool bfs(int curr, vector<vector<int>>& adjList, vector<bool>& visited) {
-        if (!visited[curr]) {
-            queue<pair<int, int>> q;
-            q.push({curr, -1});
-            visited[curr] = true;
-            while (!q.empty()) {
-                auto [curr, parent] = q.front();
-                q.pop();
-                for (int neighbor : adjList[curr]) {
-                    if (!visited[neighbor]) {
-                        visited[neighbor] = true;
-                        q.push({neighbor, curr});
-                    } else if (neighbor != parent) {
-                        return true;
-                    }
-                }
-            }
-        }
-        return false;
-    }
     bool containsCycle(vector<vector<char>>& grid) {
         int n = grid.size(), m = grid[0].size();
-        int nodes = n * m;
-        vector<vector<int>> adjList(nodes);
+        queue<pair<int, int>> q;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                int current = i * m + j;
-                if (i + 1 < n && grid[i][j] == grid[i + 1][j]) {
-                    adjList[current].push_back((i + 1) * m + j);
-                    adjList[(i + 1) * m + j].push_back(current);
+                if (grid[i][j] != '#') {
+                    q.push({i, j});
+                    char c = grid[i][j];
+                    while (!q.empty()) {
+                        auto [first, second] = q.front();
+                        if (grid[first][second] == '#')
+                            return true;
+                        grid[first][second] = '#';
+                        q.pop();
+                        if (first + 1 < n && grid[first + 1][second] == c) 
+                            q.push({first + 1, second});
+                        if (first - 1 >= 0 && grid[first - 1][second] == c) 
+                            q.push({first - 1, second});
+                        if (second + 1 < m && grid[first][second + 1] == c) 
+                            q.push({first, second + 1});
+                        if (second - 1 >= 0 && grid[first][second - 1] == c) 
+                            q.push({first, second - 1});
+                    }
                 }
-                if (j + 1 < m && grid[i][j] == grid[i][j + 1]) {
-                    adjList[current].push_back(i * m + (j + 1));
-                    adjList[i * m + (j + 1)].push_back(current);
-                }
-            }
-        }
-        vector<bool> visited(nodes, false);
-        for (int i = 0; i < nodes; i++) {
-            if (bfs(i, adjList, visited)) {
-                return true;
             }
         }
         return false;
