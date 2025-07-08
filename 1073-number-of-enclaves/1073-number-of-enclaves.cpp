9b3904ace4741419b82730cea97b06d18024cb53
Vaibhav Bichave <79131292+vaibhavbichave@users.noreply.github.com>
2025-07-08 22:59:02 +0530

    Time: 3 ms (96.11%), Space: 28.9 MB (94.91%) - LeetHub

--- 1073-number-of-enclaves/1073-number-of-enclaves.cpp
+++ 1073-number-of-enclaves/1073-number-of-enclaves.cpp
@@ -1,31 +1,32 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& board, int x, int y) {
-        board[x][y] = 0;
-        int n = board.size(), m = board[0].size();
-        if (x - 1 >= 0 && board[x - 1][y])
-            dfs(board, x - 1, y);
-        if (x + 1 < n && board[x + 1][y])
-            dfs(board, x + 1, y);
-        if (y - 1 >= 0 && board[x][y - 1])
-            dfs(board, x, y - 1);
-        if (y + 1 < m && board[x][y + 1])
-            dfs(board, x, y + 1);
-    }
     int numEnclaves(vector<vector<int>>& board) {
         int count = 0;
         int n = board.size(), m = board[0].size();
+        queue<pair<int, int>> q;
         for (int i = 0; i < n; i++) {
             if (board[i][0])
-                dfs(board, i, 0);
+                board[i][0] = 0, q.push({i, 0});
             if (board[i][m - 1])
-                dfs(board, i, m - 1);
+                board[i][m - 1] = 0, q.push({i, m - 1});
         }
         for (int j = 0; j < m; j++) {
             if (board[0][j])
-                dfs(board, 0, j);
+                board[0][j] = 0, q.push({0, j});
             if (board[n - 1][j])
-                dfs(board, n - 1, j);
+                board[n - 1][j] = 0, q.push({n - 1, j});
+        }
+        while (!q.empty()) {
+            auto [x, y] = q.front();
+            q.pop();
+            if (x - 1 >= 0 && board[x - 1][y])
+                board[x - 1][y] = 0, q.push({x - 1, y});
+            if (x + 1 < n && board[x + 1][y])
+                board[x + 1][y] = 0, q.push({x + 1, y});
+            if (y - 1 >= 0 && board[x][y - 1])
+                board[x][y - 1] = 0, q.push({x, y - 1});
+            if (y + 1 < m && board[x][y + 1])
+                board[x][y + 1] = 0, q.push({x, y + 1});
         }
         for (int i = 0; i < n; i++)
             for (int j = 0; j < m; j++)
