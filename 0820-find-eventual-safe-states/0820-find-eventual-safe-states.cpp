c2c4acd75be2930936b566bb1139c70c1c0c3de4
Vaibhav Bichave <79131292+vaibhavbichave@users.noreply.github.com>
2025-07-11 22:27:56 +0530

    Time: 48 ms (51.12%), Space: 66.3 MB (21.25%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
@@ -1,32 +1,35 @@
 class Solution {
-private:
-    bool dfs(vector<vector<int>>& graph, vector<bool>& visited,
-             vector<bool>& path, vector<bool>& topo, int node) {
-        visited[node] = true;
-        path[node] = true;
-        for (auto it : graph[node]) {
-            if (!visited[it]) {
-                if (dfs(graph, visited, path, topo, it))
-                    return true;
-            } else if (path[it])
-                return true;
-        }
-        topo[node] = true;
-        path[node] = false;
-        return false;
-    }
-
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n = graph.size();
-        vector<bool> visited(n, false), path(n, false), topo(n, false);
+        vector<int> indeg(n, 0);
+        vector<vector<int>> adj(n);
+        vector<bool> safe(n, false);
+        for (int i = 0; i < n; i++) {
+            for (int it : graph[i]) {
+                adj[it].push_back(i);
+                indeg[i]++;
+            }
+        }
+        queue<int> q;
         for (int i = 0; i < n; i++)
-            if (!visited[i])
-                dfs(graph, visited, path, topo, i);
-        vector<int> res;
+            if (indeg[i] == 0)
+                q.push(i);
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            safe[node] = true;
+            for (int it : adj[node]) {
+                --indeg[it];
+                if (indeg[it] == 0) {
+                    q.push(it);
+                }
+            }
+        }
+        vector<int> ans;
         for (int i = 0; i < n; i++)
-            if (topo[i])
-                res.push_back(i);
-        return res;
+            if (safe[i])
+                ans.push_back(i);
+        return ans;
     }
 };
