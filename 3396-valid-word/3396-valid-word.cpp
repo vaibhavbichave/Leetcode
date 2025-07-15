c6a324058892c689784aada545771d7905446095
Vaibhav Bichave <79131292+vaibhavbichave@users.noreply.github.com>
2025-07-15 09:35:32 +0530

    Time: 0 ms (100%), Space: 8 MB (67.48%) - LeetHub

--- 3396-valid-word/3396-valid-word.cpp
+++ 3396-valid-word/3396-valid-word.cpp
@@ -1,29 +1,22 @@
 class Solution {
 public:
     bool isValid(string word) {
-        int n = word.size();
-        bool set = false, set2 = false;
-        if (n < 3)
+        if (word.size() < 3) 
             return false;
-        for (char x : word) {
-            if (x >= '0' && x <= '9') {
-            } else if (x >= 'a' && x <= 'z') {
-                if (x == 'a' || x == 'e' || x == 'i' || x == 'o' || x == 'u') {
-                    set = true;
+        bool has_vowel = false;
+        bool has_consonant = false;
+        for (auto c : word) {
+            if (isalpha(c)) {
+                c = tolower(c);
+                if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                    has_vowel = true;
                 } else {
-                    set2 = true;
+                    has_consonant = true;
                 }
-            } else if (x >= 'A' && x <= 'Z') {
-                if (x == 'A' || x == 'E' || x == 'I' || x == 'O' || x == 'U') {
-                    set = true;
-                } else {
-                    set2 = true;
-                }
-
-            } else {
+            } else if (!isdigit(c)) {
                 return false;
             }
         }
-        return set && set2;
+        return has_vowel && has_consonant;
     }
 };
