c4c2391d6a1283e90d5318751ed80e4fd593e3b7
Vaibhav Bichave <79131292+vaibhavbichave@users.noreply.github.com>
2025-08-02 23:03:15 +0530

    Time: 23 ms (5.16%), Space: 39.4 MB (5.2%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
@@ -1,32 +1,14 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
- * };
- */
 class Solution {
+private:
+    string serialize(TreeNode* node) {
+        if (node)
+            return "(" + to_string(node->val) + "," + serialize(node->left) +
+                   "," + serialize(node->right) + ")";
+        return "null";
+    }
+
 public:
     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
-        if (!root)
-            return false;
-        if (isIdentical(root, subRoot))
-            return true;
-        return isSubtree(root->left, subRoot) ||
-               isSubtree(root->right, subRoot);
-    }
-    bool isIdentical(TreeNode* p, TreeNode* q) {
-        if (!p && !q)
-            return true;
-        if (!p || !q)
-            return false;
-        if (p->val != q->val)
-            return false;
-        return isIdentical(p->left, q->left) && isIdentical(p->right, q->right);
+        return serialize(root).contains(serialize(subRoot));
     }
 };
