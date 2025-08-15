c47845ab9162fb4c621bbe3b370c368348db705e
Vaibhav Bichave <79131292+vaibhavbichave@users.noreply.github.com>
2025-08-15 23:09:31 +0530

    Time: 31 ms (94.6%), Space: 20.2 MB (91.43%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
@@ -1,14 +1,33 @@
 class Solution {
 public:
+    string getString(long long n, unordered_map<long long, char>& rev,
+                     long long len) {
+        string s;
+        while (n) {
+            len /= 10;
+            s += rev[n / len];
+            n %= len;
+        }
+        return s;
+    }
     vector<string> findRepeatedDnaSequences(string s) {
-        int n = s.size();
+        unordered_map<char, long long> mp = {
+            {'A', 1}, {'C', 2}, {'G', 3}, {'T', 4}};
+        unordered_map<long long, char> rev = {
+            {1, 'A'}, {2, 'C'}, {3, 'G'}, {4, 'T'}};
+        long long n = s.size(), len = 10000000000;
+        unordered_map<long long, long long> store;
+        for (long long i = 0, key = 0; i < n; i++) {
+            key = 10 * key + mp[s[i]];
+            key %= len;
+            if (i >= 9)
+                store[key]++;
+        }
         vector<string> v;
-        unordered_map<string, int> mp;
-        for (int i = 0; i <= n - 10; i++)
-            mp[s.substr(i, 10)]++;
-        for (auto [substr, count] : mp)
+        for (auto [key, count] : store) {
             if (count > 1)
-                v.push_back(substr);
+                v.push_back(getString(key, rev, len));
+        }
         return v;
     }
 };
