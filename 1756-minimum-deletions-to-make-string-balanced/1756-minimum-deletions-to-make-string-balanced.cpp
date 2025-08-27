e8e08f9f7bdc5505567a78b39c3c195fb78a864a
Vaibhav Bichave <79131292+vaibhavbichave@users.noreply.github.com>
2025-08-27 22:23:40 +0530

    Time: 8 ms (95.85%), Space: 25.6 MB (59.86%) - LeetHub

--- 1756-minimum-deletions-to-make-string-balanced/1756-minimum-deletions-to-make-string-balanced.cpp
+++ 1756-minimum-deletions-to-make-string-balanced/1756-minimum-deletions-to-make-string-balanced.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     int minimumDeletions(string s) {
-        int n = s.size(), mini = INT_MAX;
-        vector<int> prefix(n, 0), suffix(n, 0);
-        for (int i = 1; i < n; i++)
-            prefix[i] = prefix[i - 1] + (s[i - 1] == 'b');
-        for (int i = n - 2; i >= 0; i--)
-            suffix[i] = suffix[i + 1] + (s[i + 1] == 'a');
+        int n = s.size(), mini = s.size();
+        int count_a = 0, count_b = 0;
         for (int i = 0; i < n; i++)
-            mini = min(mini, prefix[i] + suffix[i]);
+            count_a += (s[i] == 'a');
+        for (int i = 0; i < n; i++) {
+            count_a -= (s[i] == 'a');
+            mini = min(mini, count_a + count_b);
+            count_b += (s[i] == 'b');
+        }
         return mini;
     }
 };
