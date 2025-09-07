500781f525147bb26186c99c2ed1c8a02443e5ab
Vaibhav Bichave <79131292+vaibhavbichave@users.noreply.github.com>
2025-09-07 17:30:11 +0530

    Time: 0 ms (100%), Space: 9.8 MB (13.58%) - LeetHub

--- 1426-find-n-unique-integers-sum-up-to-zero/1426-find-n-unique-integers-sum-up-to-zero.cpp
+++ 1426-find-n-unique-integers-sum-up-to-zero/1426-find-n-unique-integers-sum-up-to-zero.cpp
@@ -2,12 +2,11 @@ class Solution {
 public:
     vector<int> sumZero(int n) {
         vector<int> v;
-        if (n & 1) v.push_back(0);
-        int left = -1, right = 1;
-        for (int i = 0; i < n / 2; i++) {
-            v.push_back(left--);
-            v.push_back(right++);
+        for (int i = 1; i <= n / 2; i++) {
+            v.push_back(i);
+            v.push_back(-i);
         }
+        if (n & 1) v.push_back(0);
         return v;
     }
 };
