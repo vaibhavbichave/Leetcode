c5252157d30a9de7b267af914c56b8278f4fdd49
Vaibhav Bichave <79131292+vaibhavbichave@users.noreply.github.com>
2025-09-11 22:41:49 +0530

    Time: 9 ms (81.76%), Space: 17.3 MB (20.99%) - LeetHub

--- 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.cpp
+++ 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.cpp
@@ -4,14 +4,35 @@ public:
         auto isVowel = [](char x) {
             return x == 'a' || x == 'e' || x == 'i' || x == 'o' || x == 'u';
         };
-        vector<int> index;
-        vector<char> value;
-        for (int i = 0; i < s.size(); i++)
+        int n = s.size(), c = 0;
+        vector<int> mp(256, 0);
+        for (int i = 0; i < n; i++)
             if (isVowel(tolower(s[i])))
-                index.push_back(i), value.push_back(s[i]);
-        sort(value.begin(), value.end());
-        for (int i = 0; i < value.size(); i++)
-            s[index[i]] = value[i];
+                mp[s[i]]++;
+        for (int i = 0; i < n; i++) {
+            if (isVowel(tolower(s[i]))) {
+                if (mp['A'] > 0)
+                    s[i] = 'A', mp['A']--;
+                else if (mp['E'] > 0)
+                    s[i] = 'E', mp['E']--;
+                else if (mp['I'] > 0)
+                    s[i] = 'I', mp['I']--;
+                else if (mp['O'] > 0)
+                    s[i] = 'O', mp['O']--;
+                else if (mp['U'] > 0)
+                    s[i] = 'U', mp['U']--;
+                else if (mp['a'] > 0)
+                    s[i] = 'a', mp['a']--;
+                else if (mp['e'] > 0)
+                    s[i] = 'e', mp['e']--;
+                else if (mp['i'] > 0)
+                    s[i] = 'i', mp['i']--;
+                else if (mp['o'] > 0)
+                    s[i] = 'o', mp['o']--;
+                else if (mp['u'] > 0)
+                    s[i] = 'u', mp['u']--;
+            }
+        }
         return s;
     }
 };
