ef590627c3f695252341b503121706709493eb55
Vaibhav Bichave <79131292+vaibhavbichave@users.noreply.github.com>
2026-01-04 21:48:30 +0530

    Time: 14 ms (68.23%), Space: 16.3 MB (48.28%) - LeetHub

--- 1390-four-divisors/1390-four-divisors.cpp
+++ 1390-four-divisors/1390-four-divisors.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
-    int sumFourDivisors(vector<int>& nums) {
-        int result = 0;
-        for (int num : nums) {
-            int divisors = 0, sum = 0;
-            for (int i = 1; i * i <= num; i++) {
-                if (num % i == 0) {
-                    sum += i;
+    int getSumIfExactlyFourDivisors(int num) {
+        int divisors = 0, sum = 0;
+        for (int i = 1; i * i <= num; i++) {
+            if (num % i == 0) {
+                sum += i;
+                divisors++;
+                if (i * i != num) {
+                    sum += num / i;
                     divisors++;
-                    if (i * i != num) {
-                        sum += num / i;
-                        divisors++;
-                    }
+                }
+                if (divisors > 4) {
+                    return 0;
                 }
             }
-            result += (divisors == 4) ? sum : 0;
         }
+        return (divisors == 4) ? sum : 0;
+    }
+    int sumFourDivisors(vector<int>& nums) {
+        int result = 0;
+        for (int num : nums)
+            result += getSumIfExactlyFourDivisors(num);
         return result;
     }
 };
