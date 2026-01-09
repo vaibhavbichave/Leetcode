7ce05a9eec2473e65114e490d58a78e2cb880a85
Vaibhav Bichave <79131292+vaibhavbichave@users.noreply.github.com>
2026-01-10 00:29:57 +0530

    Time: 0 ms (100%), Space: 16.2 MB (80.11%) - LeetHub

--- 0865-smallest-subtree-with-all-the-deepest-nodes/0865-smallest-subtree-with-all-the-deepest-nodes.cpp
+++ 0865-smallest-subtree-with-all-the-deepest-nodes/0865-smallest-subtree-with-all-the-deepest-nodes.cpp
@@ -12,19 +12,17 @@
  */
 class Solution {
 public:
-    int dfs(TreeNode* root, TreeNode*& lca, int& maxDepth, int depth) {
-        maxDepth = max(maxDepth, depth);
-        if (!root) return depth;
-        int l = dfs(root->left, lca, maxDepth, depth + 1);
-        int r = dfs(root->right, lca, maxDepth, depth + 1);
-        if (l == maxDepth && r == maxDepth)
-            lca = root;
-        return max(l, r);
+    pair<int, TreeNode*> dfs(TreeNode* node) {
+        if (!node) return {0, nullptr};
+        pair<int, TreeNode*> left = dfs(node->left);
+        pair<int, TreeNode*> right = dfs(node->right);
+        if (left.first > right.first) {
+            return {left.first + 1, left.second};
+        } else if (right.first > left.first) {
+            return {right.first + 1, right.second};
+        }
+        return {left.first + 1, node};
     }
-    TreeNode* subtreeWithAllDeepest(TreeNode* root) {
-        int maxDepth = 0;
-        TreeNode* lca = NULL;
-        dfs(root, lca, maxDepth, 0);
-        return lca;
-    }
+
+    TreeNode* subtreeWithAllDeepest(TreeNode* root) { return dfs(root).second; }
 };
